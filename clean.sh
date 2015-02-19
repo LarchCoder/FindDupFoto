0f38a8ac98180ac7dd38d49b4cf9506a82e3c147
ciklar <ciklar@gamil.com>
2015-02-19 22:53:34 +0100

    Initial commit: Project Framework

---
+++ clean.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -fr .cproject .project CMakeCache.txt CMakeFiles FindDupFoto Makefile bin cmake_install.cmake install_manifest.txt
---
+++ src/FindDupFoto.c
@@ -0,0 +1,14 @@
+/*
+ * FindDupFoto.c
+ *
+ *  Created on: Feb 19, 2015
+ *      Author: LarchCoder
+ */
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	printf("test\n");
+	return 1;
+}
+
+
